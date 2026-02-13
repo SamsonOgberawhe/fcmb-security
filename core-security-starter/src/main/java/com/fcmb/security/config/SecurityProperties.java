3fef71036e2fe72c424b3c086a9a65048205d75c
Samson Ogberawhe <samson.ogberawhe@jumia.com>
2026-02-13 12:48:29 +0100

    fixed module

--- core-security-starter/src/main/java/com/fcmb/security/config/SecurityProperties.java
+++ core-security-starter/src/main/java/com/fcmb/security/config/SecurityProperties.java
@@ -6,10 +6,10 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @Data
 @ConfigurationProperties(prefix = "security.jwt")
 public class SecurityProperties {
-    private String secretKey = "defaultSecretKeyForJWTSigningAndValidationPleaseChangeThis1234567890";
-    private long expirationMs = 86400000L;
-    private String issuer = "spring-security-starter";
-    private String headerName = "Authorization";
-    private String tokenPrefix = "Bearer ";
-    private boolean enableLogging = true;
+    private String secretKey;
+    private long expirationMs;
+    private String issuer;
+    private String headerName;
+    private String tokenPrefix;
+    private boolean enableLogging;
 }
